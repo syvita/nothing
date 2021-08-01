4f5778e5af453caa8f88a9f0f43376d42afc8c1b
asteria <asteria@syvita.org>
2021-08-01 19:25:03 +0100

    Update index.js

--- packages/website/src/index.js
+++ packages/website/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './components/App';
 
 Sentry.init({
